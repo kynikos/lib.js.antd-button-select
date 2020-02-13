d2e28faadeb5d23b51b63480c6e88efba7e45593
Dario Giovannetti <dev@dariogiovannetti.net>
2020-02-13 20:01:25 +0800

    Allow setting the current selection

--- index.js
+++ index.js
@@ -3,8 +3,15 @@
 // Licensed under MIT
 // https://github.com/kynikos/lib.js.antd-button-select/blob/master/LICENSE
 
-import {createElement as h, useState} from 'react'
+import {createElement as h, useState, useEffect} from 'react'
 import AntDButton from 'antd/lib/button'
+
+
+function checkSelection(multiple, selected) {
+  if (!multiple && Array.isArray(selected)) {
+    throw new Error('Set the multiple property to select more than one item.')
+  }
+}
 
 
 function handleClick({value, multiple, onChange, selected, setSelected}) {
@@ -27,12 +34,23 @@ function handleClick({value, multiple, onChange, selected, setSelected}) {
 }
 
 
-export function ButtonSelect({defaultSelected, options, multiple, size}) {
-  if (!multiple && Array.isArray(defaultSelected)) {
-    throw new Error('Set the multiple property to select more than one item.')
-  }
+export function ButtonSelect({
+  defaultSelected, selected: forceSelected, options, onChange, multiple, size,
+}) {
+  const [selected, setSelected] = useState(() => {
+    if (defaultSelected) {
+      checkSelection(multiple, defaultSelected)
+      return defaultSelected
+    }
+    return []
+  })
 
-  const [selected, setSelected] = useState(defaultSelected)
+  useEffect(() => {
+    if (forceSelected) {
+      checkSelection(multiple, forceSelected)
+      setSelected(forceSelected)
+    }
+  }, [multiple, forceSelected])
 
   let isSelected
   if (multiple) {
@@ -41,16 +59,17 @@ export function ButtonSelect({defaultSelected, options, multiple, size}) {
     isSelected = (value) => selected === value
   }
 
-  return h(AntDButton.Group, {size, style: {whiteSpace: 'nowrap'}},
-    ...options.map(([value, text]) => h(AntDButton, {
-      type: isSelected(value) ? 'primary' : 'default',
-      onClick: () => handleClick({
-        value,
-        multiple,
-        onChange,
-        selected,
-        setSelected,
-      }),
-    }, text))
-  )
+  return h(AntDButton.Group, {
+    size,
+    style: {whiteSpace: 'nowrap'},
+  }, ...options.map(([value, text]) => h(AntDButton, {
+    type: isSelected(value) ? 'primary' : 'default',
+    onClick: () => handleClick({
+      value,
+      multiple,
+      onChange,
+      selected,
+      setSelected,
+    }),
+  }, text)))
 }
